169d8d614fd00573da1fc8f131e715a84e2d39cc
Kuldeep Yeware <kuldeepyeware31@gmail.com>
2024-08-06 13:04:13 +0530

    Updated

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,12 +1,12 @@
-import AcceptedChallenge from "../_components/challenge/AcceptedChallenge";
 import AcceptedSelfChallenge from "../_components/rootPage/ActiveSelfChallenge";
+import RootAcceptedChallenge from "../_components/rootPage/RootAcceptedChallenge";
 import UserInfo from "../_components/rootPage/UserInfo";
 
 const RootPage = () => {
   return (
     <main className="space-y-4 px-6 py-10 md:px-6">
       <UserInfo />
-      <AcceptedChallenge />
+      <RootAcceptedChallenge />
       <AcceptedSelfChallenge />
     </main>
   );
